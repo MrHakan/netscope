bd58f64b5b0779b9001068970bb4e70f7eb52722
MrHakan <hakan.efe.gulacti@gmail.com>
2026-09-21 11:55:30 +0100

    Add WorkManager for host monitoring

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -108,6 +108,7 @@ dependencies {
     implementation(libs.hilt.android)
     ksp(libs.hilt.compiler)
     implementation(libs.androidx.datastore.preferences)
+    implementation(libs.androidx.work.runtime.ktx)
     implementation(libs.kotlinx.coroutines.android)
 
     debugImplementation(libs.androidx.compose.ui.tooling)
